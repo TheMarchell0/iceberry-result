b0b391b1c0f43ad161ea2ffb7f74b90de7de259c
unknown <TheMarchell0@yandex.ru>
2024-07-09 21:59:30 +0600

    initial

--- js/pages/index.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*******************************!*\
-  !*** ./src/js/pages/index.js ***!
-  \*******************************/
-console.log('TEST GLAVNAYA STRANICA');
-/******/ })()
-;
-//# sourceMappingURL=pages\index.js.map
